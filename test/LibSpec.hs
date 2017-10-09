1170d79dcb2886b92344276837fb886346230b51
elpinal <elpinal@users.noreply.github.com>
2017-10-09 23:30:35 +0900

    Update test

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -11,3 +11,4 @@ spec = do
   describe "future" $ do
     it "returns future" $ do
       future (Days 2) (Set.fromList []) `shouldBe` ([] :: [Combination Int])
+      3 `take` future (Days 2) (Set.fromList [Combination [3, 5], Combination [1, 3], Combination [6, 4]]) `shouldBe` ([] :: [Combination Int])
