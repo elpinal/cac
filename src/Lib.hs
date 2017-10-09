449a7bc9f51488c5ad669e5055b86a5f67f6d31c
elpinal <elpinal@users.noreply.github.com>
2017-10-09 22:14:31 +0900

    Add types

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,12 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+newtype Combination a = Combination { runCombination :: [a] }
+
+newtype Circulation a c = Circulation { runCirculation :: (a, c) }
+
+newtype Days = Days Int
+
+class Cycle a where
+  days :: a -> Days
